ee92ec4e64ef0e67fc8456b3dc6886e817fa71de
janu011891 <46157582+janu011891@users.noreply.github.com>
2021-12-27 07:23:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
 provider "aws" {
-  profile = "tftest"
-  region  = "ap-south-1"
+  profile = "default"
+  region  = "us-west-2"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = var.instance_name
+    Name = "ExampleTFCloudInstance"
   }
 }
